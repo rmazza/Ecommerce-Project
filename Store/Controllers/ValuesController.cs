a75db1a2d78a8202b3e88565b74a637782b5fdc7
rmazza <robertmazza1@gmail.com>
2016-10-08 14:24:00 -0500

    changed from hard coded model to an Azure database, updated controllers

--- Store/Controllers/ValuesController.cs
+++ Store/Controllers/ValuesController.cs
@@ -1,117 +1,78 @@
 ﻿using Store.Models;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.Http;
 
 namespace Store
 {
     public class ValuesController : ApiController
     {
+
         // GET api/<controller>
         public IEnumerable<ProductModel> Get()
         {
-            return new ProductModel[] {
-                new ProductModel {
-                    ID = 1,
-                    productName = "RAWLINGS RGGNP5",
-                    productPrice = 499.99m,
-                    productDescription = "The culmination of 100 years of Rawlings' glove-making craftsmanship, the new Rawlings Gold Glove series with Opti-Core™ Technology delivers the ultimate in playability and feel, inspiring a new generation of defensive excellence. These gloves feature flawless, European leather famous for its supple feel and durable finish combined with special features like hand-sewn welting and ultra-premium, luxury palm lining comfortably completes a fit and feel that can only be described as custom. These gloves are meticulously cut, assembled and sewn, start to finish, by a single craftsman, all Rawlings Gold Gloves carry an individual serial number signifying the maker, the date created and the glove's production number.",
-                    modelNumber = "RGGNP5",
-                    sport = "Baseball",
-                    category = "Baseball Gloves",
-                    size = "11.75",
-                    position = "Infield",
-                    inStock = 1,
-                    images = new ImageModel[]
-                        {
-                            new ImageModel { img = "/Images/BaseballGloves/Rawlings_RGGNP5/1.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Rawlings_RGGNP5/2.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Rawlings_RGGNP5/3.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Rawlings_RGGNP5/4.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Rawlings_RGGNP5/5.jpg" }
-                        }
-                },
-                new ProductModel {
-                    ID = 2,
-                    productName = "NOKONA P4",
-                    productPrice = 449.99m,
-                    productDescription = "Bloodline™ is back – and better than ever. A pro model glove that is highly structured and extremely lightweight. Made with Nokona’s proprietary Kangaroo leather: one of the strongest, most durable leathers in the world, yet still very light weight. Bloodline™ is re-engineered with a new interior padding system that provides superior structure, and still allows for great feel and ball control.",
-                    modelNumber = "P4",
-                    sport = "Baseball",
-                    category = "Baseball Gloves",
-                    size = "11.25",
-                    position = "Infield",
-                    inStock = 1,
-                    images = new ImageModel[]
-                        {
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P4/1.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P4/2.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P4/3.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P4/4.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P4/5.jpg" }
-                        }
-                },
-                new ProductModel {
-                    ID = 3,
-                    productName = "NOKONA P2",
-                    productPrice = 449.99m,
-                    productDescription = "Bloodline™ is back – and better than ever. A pro model glove that is highly structured and extremely lightweight. Made with Nokona’s proprietary Kangaroo leather: one of the strongest, most durable leathers in the world, yet still very light weight. Bloodline™ is re-engineered with a new interior padding system that provides superior structure, and still allows for great feel and ball control.",
-                    modelNumber = "P2",
-                    sport = "Baseball",
-                    category = "Baseball Gloves",
-                    size = "33",
-                    position = "Catcher",
-                    inStock = 1,
-                    images = new ImageModel[]
-                        {
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P2/1.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P2/2.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P2/3.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P2/4.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P2/5.jpg" }
-                        }
-                },
-                new ProductModel {
-                    ID = 4,
-                    productName = "NOKONA P5",
-                    productPrice = 449.99m,
-                    productDescription = "Bloodline™ is back – and better than ever. A pro model glove that is highly structured and extremely lightweight. Made with Nokona’s proprietary Kangaroo leather: one of the strongest, most durable leathers in the world, yet still very light weight. Bloodline™ is re-engineered with a new interior padding system that provides superior structure, and still allows for great feel and ball control.",
-                    modelNumber = "P5",
-                    sport = "Baseball",
-                    category = "Baseball Gloves",
-                    size = "11.78",
-                    position = "Infield",
-                    inStock = 1,
-                    images = new ImageModel[]
-                        {
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P5/1.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P5/2.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P5/3.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P5/4.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Nokona_P5/5.jpg" }
-                        }
-                },
-               new ProductModel {
-                    ID = 5,
-                    productName = "UNDER ARMOUR UACM-PRO1 PRO SERIES",
-                    productPrice = 379.99m,
-                    productDescription = "Wagyu cattle are known for their high quality and demand some of the highest leather prices. The hide is soft to touch but firm and durable, qualities which make a standout professional mitt.  The glove's pattern features a wide and deep pocket. Thick heel and toe padding ensure balls received don't pop out. PTH padding in the base of the hand helps prevent sting and extends the life of the mitt.  Vertical laces between the web and pocket of the mitt make the mitt much stronger and reduce chances of a snapped lace or torn web.",
-                    modelNumber = "UACM-PRO1",
-                    sport = "Baseball",
-                    category = "Baseball Gloves",
-                    size = "34",
-                    position = "Catcher",
-                    inStock = 1,
-                    images = new ImageModel[]
-                        {
-                            new ImageModel { img = "/Images/BaseballGloves/Under_Armour_UACM-PRO1/1.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Under_Armour_UACM-PRO1/2.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Under_Armour_UACM-PRO1/3.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Under_Armour_UACM-PRO1/4.jpg" },
-                            new ImageModel { img = "/Images/BaseballGloves/Under_Armour_UACM-PRO1/5.jpg" }
-                        }
+            List<ProductModel> model = new List<ProductModel>();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["StoreServer"].ConnectionString))
+            {
+
+                SqlCommand cmd = connection.CreateCommand();
+
+                cmd.CommandText = "sp_GetAllProducts";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                using(SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        ProductModel pm = new ProductModel();
+
+                        pm.ID = reader.GetInt32(reader.GetOrdinal("Id"));
+                        pm.productName = reader.GetString(reader.GetOrdinal("ProductName"));
+                        pm.productPrice = reader.GetDecimal(reader.GetOrdinal("ProductPrice"));
+                        pm.productDescription = reader.GetString(reader.GetOrdinal("ProductDescription"));
+                        pm.modelNumber = reader.GetString(reader.GetOrdinal("ModelNumber"));
+                        pm.size = reader.GetInt32(reader.GetOrdinal("Size"));
+                        pm.position = reader.GetString(reader.GetOrdinal("Position"));
+                        pm.inStock = reader.GetBoolean(reader.GetOrdinal("InStock"));
+                        pm.sport = reader.GetString(reader.GetOrdinal("Sport"));
+                        pm.productBrand = reader.GetString(reader.GetOrdinal("ProductBrand"));
+
+                        model.Add(pm);
+                    }
                 }
 
-            };
+                cmd.CommandText = "sp_GetProductImages";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                foreach(ProductModel m in model)
+                {
+                    List<ImageModel> imgModel = new List<ImageModel>();
+
+                    cmd.Parameters.AddWithValue("@prodName", m.productName);
+
+                    using(SqlDataReader imgReader = cmd.ExecuteReader())
+                    {
+                        while (imgReader.Read())
+                        {
+                            imgModel.Add(new ImageModel
+                            {
+                                img = imgReader.GetString(imgReader.GetOrdinal("ImgLink"))
+                            });
+                        }
+                    }
+                    cmd.Parameters.Clear();
+                    m.images = imgModel.ToArray();
+                }
+                //cmd.Parameters.AddWithValue("@prodName",)
+
+            }
+
+            return model;
         }
 
         // GET api/<controller>/5
--- Store/Models/ProductModel.cs
+++ Store/Models/ProductModel.cs
@@ -8,15 +8,14 @@ namespace Store.Models
     public class ProductModel
     {
         public string modelNumber { get; set; }
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string productName { get; set; }
         public string productBrand { get; set; }
         public string productDescription { get; set; }
         public string sport { get; set; }
-        public string category { get; set; }
         public decimal? productPrice { get; set; }
-        public int? inStock { get; set; }
-        public string size { get; set; }
+        public bool inStock { get; set; }
+        public int? size { get; set; }
         public string position { get; set; }
         public ImageModel[] images { get; set; }
     }
